acb9d2055a12cdb3eb174722eb0adcae81adefba
Shahar Gino <sgino@apple.com>
2020-05-09 22:52:12 +0300

    fixed

--- example_app/settings.py
+++ example_app/settings.py
@@ -1,5 +1,6 @@
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 from decouple import config
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
